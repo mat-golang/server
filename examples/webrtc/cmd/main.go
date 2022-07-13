dd7fbc2d6484e5b66b1ae818da2882a766372aca
topheruk <kristopherab@gmail.com>
2022-07-13 13:59:29 +0000

    added file server template

--- examples/webrtc/cmd/main.go
+++ examples/webrtc/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Listening to :8080")
+
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		template.Must(template.ParseFiles("pages/index.html")).Execute(w, nil)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
