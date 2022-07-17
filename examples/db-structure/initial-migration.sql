2d5bfb7f992a722bd42492b1992ca862a3970da3
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-07-17 21:16:17 +1000

    forgot semicolons

--- examples/db-structure/initial-migration.sql
+++ examples/db-structure/initial-migration.sql
@@ -5,16 +5,16 @@ CREATE TABLE users (
   user_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   username varchar(20) NOT NULL,
   email text NOT NULL
-)
+);
 
 -- Rooms are pretty much just IDs...
 CREATE TABLE rooms (
   room_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
   user_limit integer NOT NULL DEFAULT 2,
-)
+);
 
 -- Map users to which room they are in. This means we can start with 2 users per room but easily change that limit
 CREATE TABLE room_user_map (
   room_user_map_id bigserial PRIMARY KEY,
   user_id bigint REFERENCES users (user_id)
-)
+);
